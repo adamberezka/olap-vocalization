b0fb982d77c5bb60128b342ddc3412718a84d64d
adamberezka <adam.berezka99@gmail.com>
2023-05-29 22:30:04 +0200

    init

---
+++ src/main/java/pl/polsl/olapvocalization/OlapVocalizationApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.olapvocalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlapVocalizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlapVocalizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/polsl/olapvocalization/OlapVocalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.olapvocalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlapVocalizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
